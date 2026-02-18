92eea6ef48fb31abe1425307939ac853e4990e0a
tortechnocom <tortechnocom@gmail.com>
2026-02-18 16:18:07 +0700

    feat: Add confirmation code to new user data for account deletion.

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -142,6 +142,7 @@ export class AuthService {
                 data: {
                     googleId: null,
                     facebookId: null,
+                    confirmationCode,
                 },
             });
             // In production: queue a full account deletion job with the confirmationCode
